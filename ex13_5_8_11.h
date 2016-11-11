72edbe4f04a5ae189470dd4d35cde56a115dec40
sanerror <237606586@qq.com>
2016-11-11 09:09:31 +0800

    Update ex13_5_8_11.h

--- ex13_5_8_11.h
+++ ex13_5_8_11.h
@@ -5,7 +5,9 @@ public:
 	HasPtr(const std::string &s = std::string()) :
 		ps(new std::string(s)), i(0) {}
 	HasPtr(const HasPtr& a) : ps(new std::string(*(a.ps))), i(a.i) {}
-	HasPtr& HasPtr::operator=(const HasPtr& a) {
+	HasPtr& HasPtr::operator=(const HasPtr& a) { 
+		// the executing order is important to avoid some mistakes
+		// like a(a)
 		auto newp = new std::string(*(a.ps));
 		delete ps;
 		ps = newp;
