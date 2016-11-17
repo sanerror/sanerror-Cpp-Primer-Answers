6b70a7559ca0528ac602f9094a973e2cdd344d96
sanerror <237606586@qq.com>
2016-11-17 22:00:45 +0800

    Add files via upload

---
+++ ex14_16_String.cpp
@@ -0,0 +1,53 @@
+#include "ex14_16_String.h"
+#include <algorithm>
+#include <cstring>
+
+std::allocator<char> String::alloc;
+
+std::pair<char*, char*> String::alloc_n_copy(const char *a, const char *b) {
+	auto newdata = alloc.allocate(b - a + 1);
+	alloc.construct(newdata + (b - a), '\0');
+	return{ newdata, std::uninitialized_copy(a, b, newdata) };
+}
+
+void String::free() {
+	if (elements) {
+		std::for_each(elements, end, [](char& c) { alloc.destroy(&c); });
+		alloc.deallocate(elements, end - elements);
+	}
+}
+
+String::String(const char *c) {
+	const char *ce = c;
+	while (*ce)
+		++ce;
+	auto newdata = alloc_n_copy(c, ce);
+	elements = newdata.first;
+	end = newdata.second;
+}
+
+String::String(const String& s) {
+	auto newdata = alloc_n_copy(s.elements, s.end);
+	elements = newdata.first;
+	end = newdata.second;
+}
+
+String& String::operator=(const String &s) {
+	auto newdata = alloc_n_copy(s.elements, s.end);
+	free();
+	elements = newdata.first;
+	end = newdata.second;
+	return *this;
+}
+
+String::~String() {
+	free();
+}
+
+bool operator==(const String& lhs, const String& rhs) {
+	return strcmp(lhs.elements, rhs.elements);
+}
+
+bool operator!=(const String& lhs, const String& rhs) {
+	return (lhs != rhs);
+}
---
+++ ex14_16_String.h
@@ -0,0 +1,32 @@
+#ifndef EX14_16_STRING_H
+#define EX14_16_STRING_H
+
+#include <iostream>
+#include <memory>
+
+class String {
+	friend bool operator==(const String& lhs, const String& rhs);
+	friend bool operator!=(const String& lhs, const String& rhs);
+
+public:
+	String() : elements(nullptr), end(nullptr) {}
+	String(const char*);
+	String(const String&);
+	String& operator=(const String&);
+	~String();
+
+	size_t size() const { return end - elements; }
+	const char* c_str() { return elements; }
+private:
+	std::pair<char*, char*> alloc_n_copy(const char*, const char*);
+	void free();
+
+	static std::allocator<char> alloc;
+	char *elements;
+	char *end;
+};
+bool operator==(const String& lhs, const String& rhs);
+bool operator!=(const String& lhs, const String& rhs);
+
+
+#endif
